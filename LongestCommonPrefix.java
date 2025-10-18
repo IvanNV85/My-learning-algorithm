74e546e7e6341512feaa61f58d13cd9568743777
Ivan <nguyenan18385@gmail.com>
2025-10-19 00:01:22 +0700

    14. Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] string = { "flower", "flight", "flow" };
+        String[] nullString = {};
+        System.out.println(longestCommonPrefix(string));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return " ";
+        }
+        String firstString = strs[0];
+        for (int i = 0; i < firstString.length(); i++) {
+            char charToCompare = firstString.charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                String currentString = strs[j];
+                if (i == currentString.length() || currentString.charAt(i) != charToCompare) {
+                    return firstString.substring(0, i);
+                }
+
+            }
+        }
+        return firstString;
+    }
+
+}
