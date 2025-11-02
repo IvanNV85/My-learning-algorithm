7bfc1a07063c2e4e55f760e1b66ba45a8f3b8025
Ivan <nguyenan18385@gmail.com>
2025-11-02 23:55:21 +0700

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -3,25 +3,25 @@ import java.util.Arrays;
 public class ValidAnagram {
     public static void main(String[] args) {
         String s = "jar";
-        String t = t = "jam";
+        String t = "jam";
         System.out.println(isAnagram(s, t));
 
     }
 
     public static boolean isAnagram(String s, String t) {
-        //buoc 1: kiem tra do dai 2 chuoi
+        // buoc 1: kiem tra do dai 2 chuoi
         if (s.length() != t.length()) {
             return false;
         }
-        
-        //chuyen ve chuoi cac ki tu va sap xep
+
+        // chuyen ve chuoi cac ki tu va sap xep
         char[] sSort = s.toCharArray();
         char[] tSort = t.toCharArray();
 
         Arrays.sort(sSort);
         Arrays.sort(tSort);
 
-        //so sanh 2 chuoi 
+        // so sanh 2 chuoi
         return Arrays.equals(sSort, tSort);
 
     }
