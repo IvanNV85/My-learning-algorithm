ca39f1a6851a37c8581784cf427a0b76fb6ca799
Ivan <nguyenan18385@gmail.com>
2026-06-08 11:35:19 +0700

    feat: remove first node

--- algorithm/myLinkedList/LinkedList.java
+++ algorithm/myLinkedList/LinkedList.java
@@ -83,4 +83,16 @@ public class LinkedList {
         length++;
     }
 
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        if (length == 0) {
+            tail = null;
+        }
+        length--;
+        return temp;
+    }
+
 }
