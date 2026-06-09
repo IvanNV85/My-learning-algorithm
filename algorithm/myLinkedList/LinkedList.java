8afafa40c5544be237f8264b7bef55a55e0a521a
Ivan <nguyenan18385@gmail.com>
2026-06-09 11:50:41 +0700

    feat: remove a node with index

--- algorithm/myLinkedList/LinkedList.java
+++ algorithm/myLinkedList/LinkedList.java
@@ -131,4 +131,17 @@ public class LinkedList {
         return true;
     }
 
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) removeLast();
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+
 }
