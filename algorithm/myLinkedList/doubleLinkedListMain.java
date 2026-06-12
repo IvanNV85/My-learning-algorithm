791f6c1f5cd66b11d4b1cb395d9c62f4c01b200c
Ivan <nguyenan18385@gmail.com>
2026-06-12 23:33:39 +0700

    chore: setup

---
+++ algorithm/myLinkedList/doubleLinkedListMain.java
@@ -0,0 +1,9 @@
+package algorithm.myLinkedList;
+
+public class doubleLinkedListMain {
+    public static void main(String[] args) {
+        DoubleLinkedList newNode = new DoubleLinkedList(7);
+        newNode.append(8);
+        newNode.printList();
+    }
+}
