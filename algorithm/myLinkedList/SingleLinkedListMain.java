9e57aed1fbe104d78187a45bf92c222355083c8b
Ivan <nguyenan18385@gmail.com>
2026-06-14 22:06:41 +0700

    chore: test

--- algorithm/myLinkedList/SingleLinkedListMain.java
+++ algorithm/myLinkedList/SingleLinkedListMain.java
@@ -2,29 +2,36 @@ package algorithm.myLinkedList;
 
 public class SingleLinkedListMain {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(11);
+        LinkedList myLinkedList = new LinkedList(1);
 
         //before removes
-        myLinkedList.append(22);
-        myLinkedList.append(23);
-        myLinkedList.append(55);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
 //        myLinkedList.prepend(3);
 //        myLinkedList.removeFirst();
 //        myLinkedList.set(3, 55);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
-        System.out.print("List before remove: ");
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//
+        System.out.print("List: ");
         myLinkedList.printList();
+//
+//        myLinkedList.remove(2);
+//        System.out.print("\n"+ "List after remove: ");
+//        myLinkedList.printList();
 
-        myLinkedList.remove(2);
-        System.out.print("\n"+ "List after remove: ");
-        myLinkedList.printList();
-
-
+//        System.out.print("\n"+ "List after reverse: ");
+//        myLinkedList.reverse();
+//        myLinkedList.printList();
+//
 //        System.out.println("\n The number you find is: " + myLinkedList.get(1).value);
+
+        System.out.print("\n"+ "The number in the middle is: " + myLinkedList.findMiddleNode().value);
+
 
     }
 }
