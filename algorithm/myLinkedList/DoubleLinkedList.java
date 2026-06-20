fd1a0da6c8a3a48db4538904e2b11def5ab80476
Ivan <nguyenan18385@gmail.com>
2026-06-20 23:24:14 +0700

    feat: remove first in double linked list

--- algorithm/myLinkedList/DoubleLinkedList.java
+++ algorithm/myLinkedList/DoubleLinkedList.java
@@ -86,4 +86,18 @@ public class DoubleLinkedList {
         length++;
     }
 
+    public Node removeFirst(){
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
