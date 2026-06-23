ff879775733abdbf833e29f4b363bd8a0c53900a
Ivan <nguyenan18385@gmail.com>
2026-06-23 23:55:29 +0700

    learn: remove a node from a stack

--- algorithm/Stack/Stack.java
+++ algorithm/Stack/Stack.java
@@ -45,6 +45,16 @@ public class Stack {
         }
     }
 
+    public Node pop(){
+        if (height == 0) return null;
+
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        
+        return temp;
+    }
 
 
 }
