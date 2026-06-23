8ac34fe275723c62f791ff441b211150a49ea04c
Ivan <nguyenan18385@gmail.com>
2026-06-23 23:51:09 +0700

    learn: push a node into a stack

--- algorithm/Stack/Stack.java
+++ algorithm/Stack/Stack.java
@@ -35,4 +35,16 @@ public class Stack {
         }
     }
 
+    public void push(int value){
+        Node newNode = new Node(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+
+
 }
