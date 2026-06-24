a5818413cc367cd0e9f040e8d5605f3cd0b49c33
Ivan <nguyenan18385@gmail.com>
2026-06-24 23:42:45 +0700

    add a new node after first node in queue

--- algorithm/queue/Queue.java
+++ algorithm/queue/Queue.java
@@ -42,4 +42,16 @@ public class Queue {
         }
     }
 
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
 }
