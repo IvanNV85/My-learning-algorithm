d90b0fc30174ff2eab1d114494e69cb8f753db7c
Ivan <nguyenan18385@gmail.com>
2026-06-26 23:58:26 +0700

    set up to learn binary search tree

---
+++ algorithm/tree/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package algorithm.tree;
+
+public class BinarySearchTree {
+    private Node root;
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
---
+++ algorithm/tree/TreeMain.java
@@ -0,0 +1,9 @@
+package algorithm.tree;
+
+public class TreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree myTree = new BinarySearchTree();
+
+
+    }
+}
