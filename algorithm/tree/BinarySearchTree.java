31537ae23caf3bce3b41ad76b7398d8a1cea7740
Ivan <nguyenan18385@gmail.com>
2026-06-27 23:34:03 +0700

    insert a new Node into BST

--- algorithm/tree/BinarySearchTree.java
+++ algorithm/tree/BinarySearchTree.java
@@ -13,4 +13,30 @@ public class BinarySearchTree {
         }
     }
 
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+                temp.left = newNode;
+            } else {
+                if (temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
 }
