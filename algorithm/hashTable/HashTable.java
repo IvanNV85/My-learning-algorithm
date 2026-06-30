f7e48bb666d244f1f3e090c1eafcd0557d8e6eb9
Ivan <nguyenan18385@gmail.com>
2026-06-30 23:22:14 +0700

    HashTable: print HTable key value

--- algorithm/hashTable/HashTable.java
+++ algorithm/hashTable/HashTable.java
@@ -19,4 +19,15 @@ public class HashTable {
         dataMap = new Node[size];
     }
 
+    public void printTable(){
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ": ");
+            Node temp = dataMap[i];
+            while (temp != null){
+                System.out.println("  {" + temp.key + "= " + temp.value + "}");
+            }
+        }
+    }
+
+
 }
