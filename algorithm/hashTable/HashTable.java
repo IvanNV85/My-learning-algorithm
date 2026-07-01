4e458d7d747ba7cd8aa7b687d3629aaa05f84790
Ivan <nguyenan18385@gmail.com>
2026-07-01 23:55:25 +0700

    HashTable: function get all keys by an ArrayList

--- algorithm/hashTable/HashTable.java
+++ algorithm/hashTable/HashTable.java
@@ -63,5 +63,16 @@ public class HashTable {
         return 0;
     }
 
+    public ArrayList getAllkeys(){
+        ArrayList<String> allKeys = new ArrayList<>();
+        for (int i = 0; i < dataMap.length; i++) {
+            Node temp = dataMap[i];
+            while (temp != null){
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+        return allKeys;
+    }
 
 }
