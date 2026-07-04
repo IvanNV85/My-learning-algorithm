493dbf1e6e28cafd5f44a51ac9d82b910f08f142
Ivan <nguyenan18385@gmail.com>
2026-07-04 23:40:17 +0700

    Sort: learn insertion sort

---
+++ algorithm/Sort/InsertionSort.java
@@ -0,0 +1,23 @@
+package algorithm.Sort;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
