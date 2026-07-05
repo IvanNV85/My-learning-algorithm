30866bd58b5d38aefd289d10e88d44979ec2fd89
Ivan <nguyenan18385@gmail.com>
2026-07-05 23:49:39 +0700

    Sort: learn insertion sort

--- algorithm/Sort/InsertionSort.java
+++ algorithm/Sort/InsertionSort.java
@@ -1,22 +1,24 @@
 package algorithm.Sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 5, 6};
+        System.out.println("Mang truoc khi sap xep: " + Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("Mang sau khi sap xep: " + Arrays.toString(arr));
 
     }
 
     public static void insertionSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            if (i != minIndex) {
-                int temp = array[i];
-                array[i] = array[minIndex];
-                array[minIndex] = temp;
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j > -1 && temp < array[j]) {
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j--;
             }
         }
     }
