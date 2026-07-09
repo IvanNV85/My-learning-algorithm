7aa05a80877b9c75e571f1cc0ca22350019cb448
Ivan <nguyenan18385@gmail.com>
2026-07-09 23:16:14 +0700

    BSTree: breath first search function

--- algorithm/tree/BinarySearchTree.java
+++ algorithm/tree/BinarySearchTree.java
@@ -55,5 +55,24 @@ public class BinarySearchTree {
     }
 
 
+    public ArrayList<Integer> BFS(){
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+
+        while (!queue.isEmpty()){
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+
+            if (currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
+    }
 
 }
