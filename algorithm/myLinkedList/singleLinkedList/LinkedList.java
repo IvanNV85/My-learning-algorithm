dbb55e6e1be94747904f177f3099773b0140dfd4
Ivan <nguyenan18385@gmail.com>
2026-07-25 23:59:46 +0700

    SLL: selection sort

--- algorithm/myLinkedList/singleLinkedList/LinkedList.java
+++ algorithm/myLinkedList/singleLinkedList/LinkedList.java
@@ -309,5 +309,22 @@ public class LinkedList {
         head = dummy.next;
     }
 
+    public void selectionSort() {
+        if (length < 2) return;
+        Node current = head;
+        while (current.next != null) {
+            Node smallest = current;
+            Node innerCurrent = current.next;
+            while(innerCurrent != null){
+                if (innerCurrent.value < smallest.value) smallest = innerCurrent;
+                innerCurrent = innerCurrent.next;
+            }
+            if (smallest != current){
+                    int temp = 
+            }
+        }
+
+
+    }
 
 }
