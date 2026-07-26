e6a9aeb813cbc3b8657a8206c716b386c9481095
Ivan <nguyenan18385@gmail.com>
2026-07-27 00:00:43 +0700

    Array: find max and min

---
+++ algorithm/Array/FindMaxMin.java
@@ -0,0 +1,21 @@
+package algorithm.Array;
+
+public class FindMaxMin {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] findMaxMin(int[] myList) {
+        int max = myList[0];
+        int min = myList[0];
+        for (int i = 0; i < myList.length; i++) {
+            if (myList[i] > max) {
+                max = myList[i];
+            }
+            if (myList[i] < min) {
+                min = myList[i];
+            }
+        }
+        return new int[]{max, min};
+    }
+}
