9a4e9f70bfd5ac6119a41fecf79182922b6c7a0d
Ivan <nguyenan18385@gmail.com>
2026-08-13 23:55:13 +0700

    Hashtable: leetcode 350. Intersection of Two Arrays II

--- algorithm/hashTable/Intersection.java
+++ algorithm/hashTable/Intersection.java
@@ -8,38 +8,43 @@ public class Intersection {
         // Test case 1: Output mong đợi [2]
         int[] nums1_1 = {1, 2, 2, 1};
         int[] nums2_1 = {2, 2};
-        System.out.println("Test 1: " + Arrays.toString(intersection(nums1_1, nums2_1)));
+        System.out.println("Test 1: " + Arrays.toString(intersect(nums1_1, nums2_1)));
 
         // Test case 2: Output mong đợi [9,4] hoặc [4,9]
         int[] nums1_2 = {4, 9, 5};
         int[] nums2_2 = {9, 4, 9, 8, 4};
-        System.out.println("Test 2: " + Arrays.toString(intersection(nums1_2, nums2_2)));
+        System.out.println("Test 2: " + Arrays.toString(intersect(nums1_2, nums2_2)));
 
         // Test case 3: không có phần tử chung, output mong đợi []
         int[] nums1_3 = {1, 2, 3};
         int[] nums2_3 = {4, 5, 6};
-        System.out.println("Test 3: " + Arrays.toString(intersection(nums1_3, nums2_3)));
+        System.out.println("Test 3: " + Arrays.toString(intersect(nums1_3, nums2_3)));
 
     }
 
-    public static int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> resultSet = new HashSet<>();
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        //b1: luu cac value cua nums1 vao set
         for (int j : nums1) {
-            set.add(j);
+            set1.add(j);
         }
-        for (int i = 0; i < nums2.length; i++) {
-            int containsNumber = nums2[i];
-            if (set.contains(containsNumber)) {
-                resultSet.add(containsNumber);
+        //neu co phan tu trung voi set 1 thi add vao set 2
+        for (int j : nums2) {
+            if (set1.contains(j)) {
+                set2.add(j);
             }
         }
-        int[] result = new int[resultSet.size()];
+
+        //chuyen thanh mang tra ve co size = set2
+        int[] result = new int[set2.size()];
         int index = 0;
-        for (int num : resultSet) {
+        for (int num : set2) {
             result[index] = num;
             index++;
         }
+
         return result;
     }
 }
