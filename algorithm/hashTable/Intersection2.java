620bb0616479e4852a1509735e53596f635f3dca
Ivan <nguyenan18385@gmail.com>
2026-08-27 01:11:13 +0700

    Hashtable: add leetcode 350. Intersection of Two Arrays II

---
+++ algorithm/hashTable/Intersection2.java
@@ -0,0 +1,55 @@
+package algorithm.hashTable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Intersection2 {
+    public static void main(String[] args) {
+
+        // Test case 1: Output mong đợi [2,2]
+        int[] nums1_1 = {1, 2, 2, 1};
+        int[] nums2_1 = {2, 2};
+        System.out.println("Test 1: " + Arrays.toString(intersect(nums1_1, nums2_1)));
+
+        // Test case 2: Output mong đợi [9,4] hoặc [4,9]
+        int[] nums1_2 = {4, 9, 5};
+        int[] nums2_2 = {9, 4, 9, 8, 4};
+        System.out.println("Test 2: " + Arrays.toString(intersect(nums1_2, nums2_2)));
+
+        // Test case 3: không có phần tử chung, output mong đợi []
+        int[] nums1_3 = {1, 2, 3};
+        int[] nums2_3 = {4, 5, 6};
+        System.out.println("Test 3: " + Arrays.toString(intersect(nums1_3, nums2_3)));
+
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // B1: đếm số lần xuất hiện của từng số trong nums1
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // B2: duyệt nums2, nếu số đó còn "tồn dư" trong map thì lấy ra
+        List<Integer> resultList = new ArrayList<>();
+        for (int nums : nums2) {
+            //neu map
+            if (map.getOrDefault(nums, 0) > 0) {
+                resultList.add(nums);
+                map.put(nums, map.get(nums) - 1);
+            }
+        }
+
+        // B3: chuyển List sang mảng int[]
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+
+        return result;
+    }
+
+}
